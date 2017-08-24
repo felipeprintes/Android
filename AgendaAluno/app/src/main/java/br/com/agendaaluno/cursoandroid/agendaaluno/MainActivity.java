f3a9993673de864b2608f36308eab139631df349
felipeprintes <felipe.unilasalle@gmail.com>
2017-08-24 19:18:22 -0300

    layout

--- AgendaAluno/app/src/main/java/br/com/agendaaluno/cursoandroid/agendaaluno/MainActivity.java
+++ AgendaAluno/app/src/main/java/br/com/agendaaluno/cursoandroid/agendaaluno/MainActivity.java
@@ -11,12 +11,12 @@ import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.List;
 
+import br.com.agendaaluno.cursoandroid.agendaaluno.adapter.AlunosAdapter;
 import br.com.agendaaluno.cursoandroid.agendaaluno.dao.AlunoDAO;
 import br.com.agendaaluno.cursoandroid.agendaaluno.modelo.Aluno;
 
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         dao.close();
 
 
-        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        AlunosAdapter adapter = new AlunosAdapter(this, alunos);
         listaAlunos.setAdapter(adapter);
     }
 
---
+++ AgendaAluno/app/src/main/java/br/com/agendaaluno/cursoandroid/agendaaluno/adapter/AlunosAdapter.java
@@ -0,0 +1,50 @@
+package br.com.agendaaluno.cursoandroid.agendaaluno.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.agendaaluno.cursoandroid.agendaaluno.modelo.Aluno;
+
+/**
+ * Created by Printes1 on 24/08/2017.
+ */
+
+public class AlunosAdapter extends BaseAdapter {
+
+    private final List<Aluno> alunos;
+    private final Context context;
+
+    public AlunosAdapter(Context context, List<Aluno> alunos) {
+        this.context = context;
+        this.alunos = alunos;
+    }
+
+    @Override
+    public int getCount() {
+        return alunos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return alunos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return alunos.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        TextView view = new TextView(context);
+        Aluno aluno = alunos.get(position);
+        view.setText(aluno.toString());
+        return view;
+    }
+
+}

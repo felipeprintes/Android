cd5d03b50033748e11f342462f8380a2a7ef0526
felipeprintes <felipe.unilasalle@gmail.com>
2017-08-28 15:38:12 -0300

    melhorando o layout

--- AgendaAluno/app/src/main/java/br/com/agendaaluno/cursoandroid/agendaaluno/adapter/AlunosAdapter.java
+++ AgendaAluno/app/src/main/java/br/com/agendaaluno/cursoandroid/agendaaluno/adapter/AlunosAdapter.java
@@ -1,10 +1,14 @@
 package br.com.agendaaluno.cursoandroid.agendaaluno.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -46,6 +50,22 @@ public class AlunosAdapter extends BaseAdapter {
 
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.list_item, null);
+
+        TextView campoNome = (TextView) view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = (TextView) view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = (ImageView) view.findViewById(R.id.item_foto);
+
+        String caminhoFoto = aluno.getCaminhoFoto();
+        if(campoFoto != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 200, 200, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
 
         return view;
     }
